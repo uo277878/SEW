e4200d609572cab5ab6e8170928d0a4cceb98e69
Miriam González Alonso <91054158+uo277878@users.noreply.github.com>
2022-11-19 19:20:27 +0100

    Add files via upload

---
+++ Ejercicio-01/Parrafos.js
@@ -0,0 +1,12 @@
+document.write("<section>");
+document.write("<h2>Datos completos de la asignatura: </h2>");
+document.write("<p>Curso actual: ");
+document.write(asignatura.curso);
+document.write("</p>");
+document.write("<p>Alumna: ");
+document.write(asignatura.alumna);
+document.write("</p>");
+document.write("<p>e-mail: ");
+document.write(asignatura.email);
+document.write("</p>");
+document.write("</section>");
