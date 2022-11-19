5a5a32c73814ae8220a9b40535923c7edc4ec64d
Miriam González Alonso <91054158+uo277878@users.noreply.github.com>
2022-11-19 23:45:58 +0100

    Add files via upload

---
+++ Ejercicio-05/CalculadoraRPN.js
@@ -0,0 +1,157 @@
+class CalculadoraRPN{
+
+    constructor(){
+        this.pila = new Array();
+        this.pantalla = "";
+        this.puntoPulsado = false;
+        document.addEventListener("keydown", (event) => {
+            this.tecla(event.key, event);
+        });
+    }
+
+    tecla(key, evento){
+        if(key === 'c' || key === 'C'){
+            this.borrar();
+        }
+        if (key === 'x') {
+            this.multiplicacion();
+        }
+        if (key === 'v') {
+            this.division();
+        }
+        if (key === '-') {
+            this.resta();
+        }
+        if(key === '+'){
+            this.suma();
+        }
+        if(key >= '0' && key <= '9'){
+            this.digitos(Number(key));
+        }
+        if (key === '.') {
+            this.punto();
+        }
+        if (key === 'Enter') {
+            evento.preventDefault();
+            this.enter();
+        }
+    }
+
+    digitos(num){
+        this.pantalla += Number(num);
+        this.puntoPulsado = false;
+    }
+
+    punto(){
+        if(!this.puntoPulsado){
+            this.pantalla += ".";
+            this.puntoPulsado = true;
+        }
+    }
+
+    suma(){
+        if(this.pila.length >= 2){
+            this.pila.push(Number(this.pila.pop()) + Number(this.pila.pop()));
+        }
+        this.puntoPulsado = false;
+        document.querySelector('textarea').value = this.mostrar();
+    }
+
+    resta(){
+        if(this.pila.length >= 2){
+            this.pila.push(Number(this.pila.pop()) - Number(this.pila.pop()));
+        }
+        this.puntoPulsado = false;
+        document.querySelector('textarea').value = this.mostrar();
+    }
+
+    multiplicacion(){
+        if(this.pila.length >= 2){
+            this.pila.push(Number(this.pila.pop()) * Number(this.pila.pop()));
+        }
+        this.puntoPulsado = false;
+        document.querySelector('textarea').value = this.mostrar();
+    }
+
+    division(){
+        if(this.pila.length >= 2){
+            this.pila.push(Number(this.pila.pop()) / Number(this.pila.pop()));
+        }
+        this.puntoPulsado = false;
+        document.querySelector('textarea').value = this.mostrar();
+    }
+
+    sin(){
+        if(this.pila.length >= 1){
+            this.pila.push(Math.sin(Number(this.pila.pop())));
+        }
+        this.puntoPulsado = false;
+        document.querySelector('textarea').value = this.mostrar();
+    }
+
+    cos(){
+        if(this.pila.length >= 1){
+            this.pila.push(Math.cos(Number(this.pila.pop())));
+        }
+        this.puntoPulsado = false;
+        document.querySelector('textarea').value = this.mostrar();
+    }
+
+    tan(){
+        if(this.pila.length >= 1){
+            this.pila.push(Math.tan(Number(this.pila.pop())));
+        }
+        this.puntoPulsado = false;
+        document.querySelector('textarea').value = this.mostrar();
+    }
+
+    asin(){
+        if(this.pila.length >= 1){
+            this.pila.push(Math.asin(Number(this.pila.pop())));
+        }
+        this.puntoPulsado = false;
+        document.querySelector('textarea').value = this.mostrar();
+    }
+
+    acos(){
+        if(this.pila.length >= 1){
+            this.pila.push(Math.acos(Number(this.pila.pop())));
+        }
+        this.puntoPulsado = false;
+        document.querySelector('textarea').value = this.mostrar();
+    }
+
+    atan(){
+        if(this.pila.length >= 1){
+            this.pila.push(Math.atan(Number(this.pila.pop())));
+        }
+        this.puntoPulsado = false;
+        document.querySelector('textarea').value = this.mostrar();
+    }
+
+    borrar(){   
+        for(var i = 0; i <= this.pila.length; i++){
+            this.pila.pop();
+        }
+        document.querySelector('textarea').value = "";
+        this.pantalla = "";
+    }
+
+    enter(){
+        var numero = this.pantalla;
+        this.pila.push(numero);
+        this.pantalla = "";
+        document.querySelector('textarea').value = this.mostrar();
+    }
+
+    mostrar(){
+        var mostrar = ""
+        var size = this.pila.length;
+        for(var i = size-1; i >= 0; i--){
+            mostrar += i + ":\t\t" + this.pila[i] +"\n"
+        }
+        return mostrar;
+    }
+}
+
+var calculadora = new CalculadoraRPN();
